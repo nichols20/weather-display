ee9dd9f2a52b561fc46e9a7f25b83afa4a9ff593
nichols20 <johnnytes10@gmail.com>
2021-10-19 15:07:55 -0400

    installed axios npm package

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light ">
+        <p>This is the navbar</p>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
