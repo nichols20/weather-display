429e50f9e7d1f4a92909484028410cdafaf7cbfe
nichols20 <johnnytes10@gmail.com>
2021-11-07 13:11:05 -0500

    get weather data solution

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,25 +8,35 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get("/weatherquery/:zip", (req, res) => {
+app.get("/weatherquery/:zip", async (req, res) => {
   const config = {
     method: "get",
     url: `https://api.openweathermap.org/data/2.5/weather?zip=${req.params.zip}&appid=${apiKey}`,
     headers: {},
   };
 
-  axios(config)
+  const weatherQuery = await axios(config)
     .then(function (response) {
-      console.log(
-        JSON.stringify(response.data),
-        res.send(JSON.stringify(response.data.main))
-      );
+      const { temp, feels_like, temp_min, temp_max, humidity } =
+        response.data.main;
+      res.send(JSON.stringify(response.data.main));
+      const result = {
+        temp: temp,
+        feelsLike: feels_like,
+        minTemp: temp_min,
+        maxTemp: temp_max,
+        humidity: humidity,
+      };
+      return result;
     })
     .catch(function (error) {
       console.log(error);
     });
+
+  //send weatherquery out of scope to be intercepted by other methods
+  //so that weather query data may be used
 });
 
-app.listen(80, () => console.log("listening on port 80"));
+app.listen(90, () => console.log("listening on port 80"));
 
 module.exports = app;
